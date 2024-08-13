05368fa8227d75223bdbe18fa8fe5fc51a2df638
dohoangnam2912 <dohoangnam2912@gmail.com>
2024-08-13 17:58:58 +0700

    Next update

--- model.py
+++ model.py
@@ -26,12 +26,12 @@ class PositionalEncoding(nn.Module):
         super().__init__()
         self.d_model = d_model
         self.seq_len = seq_len
-        self.droupout = dropout
+        self.dropout = nn.Dropout(dropout)
 
         # Create a matrix of shape (seq_len, d_model)
         pe = torch.zeros(seq_len, d_model)
         # Create a vector of shape (seq_len, 1)
-        position = torch.arrange(0, seq_len, dtype=torch.flat).unsqueeze(1)
+        position = torch.arange(0, seq_len, dtype=torch.flat).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model,2).float() * (-math.log(10000.0) / d_model))
         # Apply the sin to even positions
         pe[:, 0::2] = torch.sin(position * div_term)
@@ -60,3 +60,114 @@ class LayerNormalization(nn.Module):
         mean = x.mean(dim = -1, keepdim=True) #Last dimension
         std = x.std(dim = -1, keepdim=True)
         return self.alpha*(x - mean) / (std + self.eps) + self.bias
+    
+class FeedForwardBlock(nn.Module):
+    """
+    The feedforward block in both encoder and decoder, with different weights and biases
+    The input sentence is a tensor with Dimension batch and sequence length and d_model
+    """
+    def __init__(self, d_model:int, d_ff:int, dropout:float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff) #W1 and B1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model) #W2 and B2
+
+    def forward(self, x):
+        # (Batch, seq_len, d_model) ----> (Batch, seq_len, d_ff) ----> (Batch, seq_len, d_model) 
+        return self.linear_2(self.dropout(torch.ReLU(self.linear_1(x))))
+    
+class MultiHeadAttentionBlock(nn.Module):
+    """
+    Three inputs: query, key, values (Duplication of the inputs three times)
+    h: Number of head
+    mask: If we want a word that does not interact with other words
+    """
+
+    def __init__(self, d_model:int, h:int, dropout:float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        self.dropout = nn.Dropout(dropout)
+        
+        assert d_model % h == 0, "d_model is not divisible by h"
+
+        self.d_k = d_model // h 
+        self.w_q = nn.Linear(d_model, d_model) #Wq
+        self.w_k = nn.Linear(d_model, d_model) #Wk
+        self.w_v = nn.Linear(d_model, d_model) #Wv 
+
+        self.w_o = nn.Linear(d_model, d_model)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout=nn.Dropout):
+        d_k = query.shape[-1]
+
+        # (Batch, h, Seq_len, d_k) --> (Batch, h, Seq_len, Seq_len)
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k) #Transpose the last 2 dimensions 
+        if mask is not None:
+            attention_scores.masked_fill_(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(dim = -1) #(Batch, h, seq_len, seq_len)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        return (attention_scores @ value), attention_scores
+
+
+
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q) # (Batch, Seq_len, d_model) --> (Batch, Seq_len, d_model)
+        key = self.w_k(k) # (Batch, Seq_len, d_model) --> (Batch, Seq_len, d_model)
+        value = self.w_v(v) # (Batch, Seq_len, d_model) --> (Batch, Seq_len, d_model)
+
+
+        # (Batch, Seq_len, d_model) --> (Batch, Seq_len, h, d_k) --> (Batch, h, Seq_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2) #Split the query into h of dk
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1 , 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout) #Why do I need to use staticmethod here?
+
+        # (Batch, h, Seq_len, d_k) --> (Batch, Seq_len, h, d_k) --> (Batch, Seq_len, d_model)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k) #Why do i need contiguous here?, what is the value -1 here
+
+        # (Batch, Seq_len, d_model) --> (Batch, Seq_len, d_model)
+        return self.w_o(x)
+    
+class ResidualConnection(nn.Module):
+
+    def __init__(self, dropout: float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
+    
+class EncoderBlock(nn.Module):
+    """
+    Combine every subblock that we have defined
+    src_mask: The mask we want to apply to input of the encoder to hide the interaction of the padding words 
+    with other words we don't want the padding word to interact
+    """
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)]) #Organize a list of module
+    
+    def forward(self, x, src_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask)) #The query coming from the decoder are watching the key and the values coming from the encoder
+        x = self.residual_connections[1](x, self.feed_forward_block)
+        return x
+
+class Encoder(nn.Module):
+
+    def __init__(self, layers: nn.Module) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
